fdae7c321b1532bab8a4fe328ef599d1c1e62be7
Santino Di Gennaro <santinodg96@gmail.com>
2022-11-26 13:10:01 +0100

    faq dropdown

--- faq.js
+++ faq.js
@@ -3,16 +3,21 @@ const question = document.getElementsByClassName('question');
 for(let  i = 0; i<question.length; i++) {
     question[i].addEventListener('click', () => {
     const answer = document.getElementsByClassName('answer');
+    const cross = document.getElementsByClassName('cross');
 
     if(answer[i].classList.contains('invisible')) {
         for(let  i = 0; i<question.length; i++) {
             answer[i].classList.add('invisible');
+            cross[i].classList.remove('rotate');
         }
 
         answer[i].classList.remove('invisible');
+        cross[i].classList.add('rotate');
         return;
     }
     
     answer[i].classList.add('invisible');
+    cross[i].classList.remove('rotate');
 });
 }
+
